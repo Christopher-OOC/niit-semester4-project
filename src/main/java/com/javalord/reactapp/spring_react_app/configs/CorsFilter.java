259609facdd2e62bfd4148a926c78cd7610c22ac
Christopher-OOC <olojedechristopher24@gmail.com>
2025-06-11 20:03:28 +0100

    commit

---
+++ src/main/java/com/javalord/reactapp/spring_react_app/configs/CorsFilter.java
@@ -0,0 +1,22 @@
+package com.javalord.reactapp.spring_react_app.configs;
+
+import org.springframework.stereotype.Component;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+    }
+}
