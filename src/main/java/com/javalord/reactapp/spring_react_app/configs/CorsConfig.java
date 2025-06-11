59d806a42d34bd88711df60a5202542c7de70613
Christopher-OOC <olojedechristopher24@gmail.com>
2025-06-11 19:42:32 +0100

    commit

--- src/main/java/com/javalord/reactapp/spring_react_app/configs/CorsConfig.java
+++ src/main/java/com/javalord/reactapp/spring_react_app/configs/CorsConfig.java
@@ -11,10 +11,11 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")  // Apply to all endpoints
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:5173", "https://frontend-five-ebon-62.vercel.app", "http://localhost:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("*")
+                        .allowCredentials(true)
                         .maxAge(10000);
     }
 }
